156cee4d52debca74f1d654b3ca4a20e99890309
yorammi <yorammi@tikalk.com>
2019-01-12 19:46:32 +0200

    Add wicDemo application

---
+++ apps/maven-app/src/main/java/tci/apps/maven/wicDemo.java
@@ -0,0 +1,31 @@
+package tci.apps.maven;
+
+public class wicDemo {
+    public static void main( String[] args ) {
+        winterIsComing();
+    }
+    public static void winterIsComing() {
+        System.out.println( " ");
+        System.out.println( " ");
+        System.out.println( "W    W    W III N  N TTT EEE RRR" );
+        System.out.println( "W    W    W  I  NN N  T  E   R R" );
+        System.out.println( " W  W W  W   I  NNNN  T  EEE RR " );
+        System.out.println( " W  W W  W   I  N NN  T  E   R R" );
+        System.out.println( "  WW   WW   III N NN  T  EEE R R" );
+        System.out.println( " ");
+        System.out.println( "      III  SSS" );
+        System.out.println( "       I  S   " );
+        System.out.println( "       I   S" );
+        System.out.println( "       I    S" );
+        System.out.println( "       I     S" );
+        System.out.println( "      III SSS" );
+        System.out.println( " ");
+        System.out.println( " CCC  OO   MM MM III N  N  GGG" );
+        System.out.println( "C    O  O  M M M  I  NN N G   " );
+        System.out.println( "C    O  O  M M M  I  NNNN G GG" );
+        System.out.println( "C    O  O  M   M  I  N NN G  G" );
+        System.out.println( " CCC  OO   M   M III N  N  GGG" );
+        System.out.println( " ");
+        System.out.println( " ");
+    }
+}
---
+++ apps/maven-app/src/test/java/tci/apps/maven/wicDemoTest.java
@@ -0,0 +1,14 @@
+package tci.apps.maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class wicDemoTest extends TestCase
+{
+    wicDemo demo;
+
+    public void testWicTest() {
+        demo.winterIsComing();
+    }
+}
